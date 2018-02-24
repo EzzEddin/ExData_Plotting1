423d40b0d90ffc727ebd39487e063791088a3461
EzzEddin <EzzEddin@users.noreply.github.com>
2018-02-24 17:32:43 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,6 @@
 # Generating the first plot
 dat <- read.table("household_power_consumption.txt"
                   , header = TRUE, sep = ";", na.strings = "?")
-                  # , colClasses = c("Date", "POSIct", "numeric",
-                  #                  "numeric", "numeric", "numeric",
-                  #                  "numeric", "numeric", "numeric"))
 datReq <- subset(dat, Date %in% "2/2/2007" | Date %in% "1/2/2007")
 hist(as.numeric(datReq$Global_active_power), col = "red"
      , main = "Global Active Power", xlab = "Global Active Power (killowatts)")
